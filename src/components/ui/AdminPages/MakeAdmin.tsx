141b8749ecc6220454a9051f7cb1a273c2ec2db3
GHSoyad <ghsoyad@gmail.com>
2024-02-14 02:20:33 +0600

    Add all users page with make user admin

---
+++ src/components/ui/AdminPages/MakeAdmin.tsx
@@ -0,0 +1,80 @@
+"use client"
+import { makeUserAdmin } from '@/utils/actions/makeUserAdmin';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+
+const MakeAdmin = ({ id, setUsers }: { id: string, setUsers: any }) => {
+  const [formLoading, setFormLoading] = useState(false);
+  const [confirmation, setConfirmation] = useState(false);
+
+  useEffect(() => {
+    if (confirmation) {
+      setFormLoading(true);
+
+      makeUserAdmin(id)
+        .then(data => {
+          if (data.success) {
+            toast.success(data.message);
+            setUsers((users: any) => (
+              users.map((user: any) => {
+                if (user._id === id) {
+                  return {
+                    ...user,
+                    role: "admin"
+                  }
+                } else {
+                  return user
+                }
+              })
+            ))
+          } else {
+            toast.error(data.message);
+          }
+        })
+        .catch(err => console.log(err))
+        .finally(() => {
+          setFormLoading(false);
+          setConfirmation(false);
+        });
+    }
+    //eslint-disable-next-line
+  }, [confirmation]);
+
+  const handleSubmit = () => {
+    toast((t) => (
+      <div className='text-center p-6'>
+        <div>
+          <b>Are you sure you want to make this user Admin?</b>
+        </div>
+        <button
+          className='btn btn-sm border-primary border-2 bg-white mt-4 hover:text-white hover:bg-primary me-2'
+          onClick={() => { setConfirmation(true), toast.dismiss(t.id) }}
+        >
+          Confirm
+        </button>
+        <button
+          className='btn btn-sm border-red-600 border-2 bg-white text-red-600 mt-4 hover:text-white hover:bg-red-600'
+          onClick={() => { setConfirmation(false), toast.dismiss(t.id) }}
+        >
+          Cancel
+        </button>
+      </div>
+    ),
+      {
+        duration: Infinity
+      });
+  }
+
+  return (
+    <button
+      onClick={handleSubmit}
+      title="donate" className="text-sm font-medium rounded-lg p-2 border-primary border-2 bg-white mt-4 hover:text-white hover:bg-primary"
+      disabled={formLoading}
+    >
+      Make Admin
+    </button>
+  );
+};
+
+export default MakeAdmin;
---
+++ src/utils/actions/makeUserAdmin.ts
@@ -0,0 +1,25 @@
+"use server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../authOptions";
+import { revalidateTag } from "next/cache";
+
+export const makeUserAdmin = async (id: string) => {
+  const session = await getServerSession(authOptions);
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/admin-user/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${session?.token}`
+    },
+    body: JSON.stringify({}),
+    cache: "no-cache",
+  });
+
+  const userInfo = await res.json();
+  if (userInfo.success) {
+    revalidateTag('makeUserAdmin');
+  }
+
+  return userInfo;
+}
