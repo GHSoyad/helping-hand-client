b898c983b20ac63b69abbb83461a227cc27cbdfb
GHSoyad <ghsoyad@gmail.com>
2024-02-14 02:54:21 +0600

    Add featured donations section for home page

---
+++ src/components/ui/Homepage/FeaturedDonations.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import DonationCard from '../DonationsPage/DonationCard';
+import { DonationInterface } from '@/types/globalTypes';
+import Link from 'next/link';
+
+const FeaturedDonations = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/donations/featured`);
+  const data = await res.json();
+  const donations: DonationInterface[] = data.content;
+
+  return (
+    <div className='text-center'>
+      <h1 className='text-3xl font-bold text-primary my-10'>Featured Donations</h1>
+      {
+        donations?.length &&
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+          {
+            donations?.map(donation => (
+              <DonationCard key={donation?._id} donation={donation} />
+            ))
+          }
+        </div>
+      }
+      <Link href='/donations'>
+        <button
+          title="donation-page" className="btn border-primary border-2 bg-white hover:text-white hover:bg-primary my-10"
+        >
+          View More
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default FeaturedDonations;
